df1b83476dceda41dde9763afe8a705e5ef7874d
Alexander <ruzhickiisasha@gmail.com>
2022-01-06 14:33:39 +0200

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,1 +1,51 @@
-
+const data = {
+    "first_name": "Alexander",
+    "last_name": "Ruzhickii",
+    "job_title": "FrontEnd Developer",
+    "photo": "images/w1.jpg",
+    "phone": "+3 80631689372",
+    "email": "ruzhickiisasha@gmail.com",
+    "skype": "ruzhickii",
+    "linkedin": "https://www.linkedin.com/in/alexander-ruzhickii-4a6004116/",
+    "github": "https://github.com/ruzhickii",
+    "address": "Odessa, Ukraine",
+    "skills": [
+        "Html5",
+        "Css3",
+        "JavaScript",
+        "ES6",
+        "SCSS",
+        "SASS",
+        "BEM",
+        "Flexbox",
+        "Grid",
+        "Pug/Jade",
+        "Inline css",
+        "Git",
+        "SVN",
+        "Adobe Photoshop",
+        "Zeplin",
+        "Avocode",
+        "Bootstrap",
+        "Material UI",
+        "Gulp",
+        "NPM",
+        "Webpack",
+        "React JS",
+        "Next JS",
+        "Redux",
+        "RESTful API",
+        "Animate.css"
+    ],
+    "education": [
+        ["2005-2010", "Odessa Law University"],
+        ["2015", "ItStep Academy - Software Development."],
+        ["2016", "ITEA Education Academy"],
+        ["2017", "Beetroot Academy"]
+    ],
+    "achievements": [
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation",
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation"
+    ],
+    "profile": "Diligence, assiduity, conscientious attitude to work, honesty, responsibility and serious attitude to any deal, fast studying with a new program product and technology, flexible and stress resistant person."
+};
