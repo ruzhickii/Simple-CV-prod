66269d3fb221987fb398ce5c18fa5831ae328754
Alexander <ruzhickiisasha@gmail.com>
2022-01-06 23:06:41 +0200

    Update experience.js

--- js/experience.js
+++ js/experience.js
@@ -1,12 +1,12 @@
 const experience = [
     {
-        "company": "ITTeam",
-        "city": "Kiev",
-        "from": "2016",
-        "to": "2017",
-        "position": "FrontEnd developer",
-        "description": "PSD to HTML5, PSD to React, UI Components, Admin UI"
-    },
+        "company": "InfoEd Global",
+        "city": "Albany, New York Area",
+        "from": "2019",
+        "to": "2021",
+        "position": "FrontEnd Engineer",
+        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
+    }, 
     {
         "company": "PerceptionBox",
         "city": "Odessa",
@@ -16,11 +16,11 @@ const experience = [
         "description": "HTML5, CSS3, SCSS, React, Next, WordPress"
     },
     {
-        "company": "InfoEd Global",
-        "city": "Albany, New York Area",
-        "from": "2019",
-        "to": "2021",
-        "position": "FrontEnd Engineer",
-        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
-    }  
+        "company": "ITTeam",
+        "city": "Kiev",
+        "from": "2016",
+        "to": "2017",
+        "position": "FrontEnd developer",
+        "description": "PSD to HTML5, PSD to React, UI Components, Admin UI"
+    },   
 ]
