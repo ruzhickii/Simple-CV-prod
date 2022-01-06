36d880d64c494544a5b3505f39a8f29db75f3c33
Alexander <ruzhickiisasha@gmail.com>
2022-01-06 14:33:54 +0200

    Add files via upload

--- js/experience.js
+++ js/experience.js
@@ -1,1 +1,26 @@
-
+const experience = [
+    {
+        "company": "ITTeam",
+        "city": "Kiev",
+        "from": "2016",
+        "to": "2017",
+        "position": "FrontEnd developer",
+        "description": "PSD to HTML5, PSD to React, UI Components, Admin UI"
+    },
+    {
+        "company": "PerceptionBox",
+        "city": "Odessa",
+        "from": "2017",
+        "to": "2017",
+        "position": "FrontEnd developer",
+        "description": "HTML5, CSS3, SCSS, React, Next, WordPress"
+    },
+    {
+        "company": "InfoEd Global",
+        "city": "Albany, New York Area",
+        "from": "2019",
+        "to": "2021",
+        "position": "FrontEnd Engineer",
+        "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
+    }  
+]
